6744a9bca377aae6828d3d50c7f2262e2cc20295
ramoncordova <ramon.cordova@lcliquidezcorporativa.com.mx>
2021-08-23 16:59:34 -0700

    funciones
    parte de funciones del curso

---
+++ descuentos.js
@@ -0,0 +1,18 @@
+// const precioOriginal = 120;
+// const descuento = 18;
+
+
+function calcularPrecioConDescuento(precio, descuento){
+    const porcentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+
+    return precioConDescuento;
+}
+
+
+
+
+
+// console.log({
+//     precioOriginal, descuento, porcentajePrecioConDescuento, precioConDescuento
+// });
